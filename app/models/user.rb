d43c3a99b4786a9ea738922e1431f726e9321ed5
fgxut <fgxut1206@gmail.com>
2019-06-23 15:02:55 +0000

    Add user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,22 @@
 class User < ApplicationRecord
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships,
+        class_name: 'Relationship',
+       foreign_key: :follower_id,
+         dependent: :destroy
+  has_many :passive_relationships,
+        class_name: 'Relationship',
+       foreign_key: :followed_id,
+         dependent: :destroy
+  # @user.active_relationships.map(&:followed)
+  # @user.following
+  has_many :following,
+    through: 'active_relationships',
+     source: 'followed'
+  has_many :followers,
+    through: 'passive_relationships',
+     source: 'follower'
+
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :downcase_email
   before_create :create_activation_digest
@@ -41,50 +58,62 @@ class User < ApplicationRecord
     BCrypt::Password.new(digest).is_password?(token)
   end
 
-  # アカウントを有効にする
   def activate
     update_attribute(:activated,    true)
     update_attribute(:activated_at, Time.zone.now)
   end
 
-  # 有効化用のメールを送信する
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
   end
 
-  # パスワード再設定の属性を設定する
   def create_reset_digest
     self.reset_token = User.new_token
     update_attribute(:reset_digest,  User.digest(reset_token))
     update_attribute(:reset_sent_at, Time.zone.now)
   end
 
-  # パスワード再設定のメールを送信する
   def send_password_reset_email
     UserMailer.password_reset(self).deliver_now
   end
-  
-  # パスワード再設定の期限が切れている場合はtrueを返す
+
   def password_reset_expired?
     reset_sent_at < 2.hours.ago
   end
-  
+
   # 試作feedの定義
   # 完全な実装は次章の「ユーザーをフォローする」を参照
+  # current_user.feed
+  # current_user.id
+  # current_user.microposts
   def feed
-    Micropost.where("user_id = ?", id)
+    following_ids = "SELECT followed_id FROM relationships
+                     WHERE follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids}) OR user_id = :user_id",
+                                 user_id: self.id)
+  end
+
+  def follow(other_user)
+    self.active_relationships.create(followed_id: other_user.id)
+  end
+
+  def unfollow(other_user)
+    self.active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  def following?(other_user)
+    self.following.include?(other_user)
   end
 
   private
 
-    # メールアドレスをすべて小文字にする
     def downcase_email
-      self.email = email.downcase
+      self.email = self.email.downcase
     end
 
-    # 有効化トークンとダイジェストを作成および代入する
     def create_activation_digest
       self.activation_token  = User.new_token
-      self.activation_digest = User.digest(activation_token)
+      self.activation_digest = User.digest(self.activation_token)
+      # @user.activation_digest => ハッシュ値
     end
 end
--- test/controllers/microposts_controller_test.rb
+++ test/controllers/microposts_controller_test.rb
@@ -1,7 +1,6 @@
 require 'test_helper'
 
 class MicropostsControllerTest < ActionDispatch::IntegrationTest
-
   def setup
     @micropost = microposts(:orange)
   end
@@ -19,7 +18,7 @@ class MicropostsControllerTest < ActionDispatch::IntegrationTest
     end
     assert_redirected_to login_url
   end
-  
+
   test "should redirect destroy for wrong micropost" do
     log_in_as(users(:michael))
     micropost = microposts(:ants)
@@ -28,4 +27,5 @@ class MicropostsControllerTest < ActionDispatch::IntegrationTest
     end
     assert_redirected_to root_url
   end
+
 end
